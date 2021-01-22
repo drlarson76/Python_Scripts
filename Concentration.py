e5de2547f01ec9743028708c790676e3934f9072
drlarson76 <77848283+drlarson76@users.noreply.github.com>
2021-01-22 12:08:37 -0700

    Update Concentration.py

--- Concentration.py
+++ Concentration.py
@@ -1,8 +1,8 @@
 # Concentration
-##Finds the concentration of an additive in a container
-##with equal input and output flow based on initial concentration
-##and input concentration.
-##22 Nov 2020 drl
+##  Finds the concentration of an additive in a container
+##  with equal input and output flow based on initial concentration
+##  and input concentration.
+##  22 Nov 2020 drl
 
 import matplotlib.pylab as plt
 import numpy as np
@@ -10,16 +10,15 @@ import numpy as np
 # Case 1.  Input is plain water.  Container has initial concentraion.
 CInput = 0      # Input flow concentration
 COutput = 1     # Intial concentration in container
-C = [COutput]   # Initialize list
+Concentration = [COutput]   # Initialize list, container Concentration
 a = 0.1         # Flow rate as fraction of container capacity
 NN = 50         # Number of iterations of fixed delta t
 
 for jj in range(0, NN):
     COutput = COutput*(1-a) + CInput*(a)
-    C.append(COutput)
+    Concentration.append(COutput)
 
-
-plt.plot(C)
+plt.plot(Concentration)
 plt.title('Case 1.  Plain water in.  Container starts with additive.') 
 plt.figure()
 
@@ -27,45 +26,42 @@ plt.figure()
 #            plain water.
 CInput = 1
 COutput = 0
-C2 = [COutput]
+Concentration2 = [COutput]
 # a = 0.1
 
 for jj in range(0, NN):
-    # CLast = CLast*(1-a)
     COutput = COutput*(1-a) + CInput*(a)
-    C2.append(COutput)
+    Concentration2.append(COutput)
 
-plt.plot(C2)
+plt.plot(Concentration2)
 plt.title('Case 2.  Constant additive in.  Container starts with plain water.') 
 plt.figure()
 
 # Compare Case 1 and Case 2
 MOnes = -1*np.ones(NN+1)
-C3 = np.array(C) + np.array(C2) + MOnes # Numpy arrays add element by element.
+Concentration3 = np.array(Concentration) + np.array(Concentration2) + MOnes # Numpy arrays add element by element.
 # Alternatively np.add will add lists element by element, but only 2 lists
-# C3 = np.add(C, C2)
-# C3 = np.add(C3, MOnes)
-plt.plot(C3)
+# Concentration3 = np.add(Concentration, Concentration2)
+# Concentration3 = np.add(Concentration3, MOnes)
+plt.plot(Concentration3)
 plt.title('Compare Case 1 and Case 2 Curve Shape') 
 plt.figure()
 
-
+# Case 3.  Input toggles between constant concentration as input and plain water,
+#            container initially has plain water.
 
 CInput = 1
 COutput = 0
-C = [COutput]
-# a = 0.1
+Concentration = [COutput]
 
-# Case 3.  Input toggles between constant concentration and plain water,
-#            container initially has plain water.
 for jj in range(0, NN):
     # CLast = CLast*(1-a)
     if jj % 2 == 0:
         COutput = COutput*(1-a) + CInput*(a)
     else:
         COutput = COutput*(1-a)
-    C.append(COutput)
+    Concentration.append(COutput)
 
-plt.plot(C)
+plt.plot(Concentration)
 plt.title('Case 3.  Input toggles between additive and no additive. \n Container starts with plain water.') 
 plt.show()
